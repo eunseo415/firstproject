4d1ed12f47392bebe7eda68855d596eb7ca23c78
eunseo415 <eskim150405@gamil.com>
2025-09-17 19:08:43 +0900

    4강

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@ToString
 public class Article {
     @Id              //엔티티 대푯값 지정
     @GeneratedValue  //자동 생성 기능
@@ -15,18 +19,5 @@ public class Article {
     @Column
     private String content;
 //Article 생성자 추가
-    public Article(Long id,String title,String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-//toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+
 }
