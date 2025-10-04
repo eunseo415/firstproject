b3de53c3413635eb6d26099de5a860355071c07b
eunseo415 <eskim150405@gamil.com>
2025-10-04 14:37:23 +0900

    11강 API

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -4,10 +4,12 @@ import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public interface ArticleRepository extends CrudRepository<Article, Long>
 // 2개의 제너릭 요소를 받음
 {
     @Override
     ArrayList<Article> findAll();
+
 }
